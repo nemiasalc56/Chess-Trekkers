b8b8f947bea640f7940c91364ea4cf7553f6b236
Krystina O'Connor <krystina.m.oconnor@gmail.com>
2020-05-04 16:19:49 -0500

    added grid for chess

---
+++ src/ChessBoard/index.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import './index.css';
+
+function ChessBoard() {
+  return (
+    	<div className="board">
+			<div className="top">
+		    	<div className="board-inner">
+		      		<div className="box-inner">A</div>
+		      		<div className="box-inner">B</div>
+		      		<div className="box-inner">C</div>
+		      		<div className="box-inner">D</div>
+		      		<div className="box-inner">E</div>
+		      		<div className="box-inner">F</div>
+		      		<div className="box-inner">G</div>
+		      		<div className="box-inner">H</div>
+		    	</div>
+		  	</div>
+		  	<div className="box"></div>
+		  	<div className="box"></div>
+		  	<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+			<div className="box"></div>
+
+			<div className="bottom">
+		    	<div className="board-inner">
+		      		<div className="box-inner">A</div>
+		      		<div className="box-inner">B</div>
+		      		<div className="box-inner">C</div>
+		      		<div className="box-inner">D</div>
+		      		<div className="box-inner">E</div>
+		      		<div className="box-inner">F</div>
+		      		<div className="box-inner">G</div>
+		      		<div className="box-inner">H</div>
+		    	</div>
+		  	</div>
+		</div>
+  )
+}
+
+export default ChessBoard;
