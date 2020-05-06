7d87cbd19396034b5e72f52421a70dd05fc8fcbc
roberto cortes <robertcortesgrimm223@gmail.com>
2020-05-05 20:13:35 -0500

    changed board component to smart component

--- src/ChessBoard/index.js
+++ src/ChessBoard/index.js
@@ -1,107 +1,121 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './index.css';
 
-function ChessBoard() {
-  return (
-    	<div className="board">
-			<div className="top">
-		    	<div className="board-inner">
-		      		<div className="box-inner">A</div>
-		      		<div className="box-inner">B</div>
-		      		<div className="box-inner">C</div>
-		      		<div className="box-inner">D</div>
-		      		<div className="box-inner">E</div>
-		      		<div className="box-inner">F</div>
-		      		<div className="box-inner">G</div>
-		      		<div className="box-inner">H</div>
-		    	</div>
-		  	</div>
-		  	<div className="box"></div>
-		  	<div className="box"></div>
-		  	<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
+export default class ChessBoard extends Component {
+	constructor(props){
+		super(props)
 
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
+		this.state = { 
+			movement: null 
 
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
+		}
+	}
 
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
+	render(){
 
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
+	  return (
+	    	<div className="board">
+				<div className="top">
+			    	<div className="board-inner">
+			      		<div className="box-inner">A</div>
+			      		<div className="box-inner">B</div>
+			      		<div className="box-inner">C</div>
+			      		<div className="box-inner">D</div>
+			      		<div className="box-inner">E</div>
+			      		<div className="box-inner">F</div>
+			      		<div className="box-inner">G</div>
+			      		<div className="box-inner">H</div>
+			    	</div>
+			  	</div>
+			  	<div className="box"><div className="dumb-box"></div></div>
+			  	<div className="box"></div>
+			  	<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
 
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
 
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
 
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
-			<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
 
-			<div className="bottom">
-		    	<div className="board-inner">
-		      		<div className="box-inner">A</div>
-		      		<div className="box-inner">B</div>
-		      		<div className="box-inner">C</div>
-		      		<div className="box-inner">D</div>
-		      		<div className="box-inner">E</div>
-		      		<div className="box-inner">F</div>
-		      		<div className="box-inner">G</div>
-		      		<div className="box-inner">H</div>
-		    	</div>
-		  	</div>
-		</div>
-  )
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+				<div className="box"></div>
+
+				<div className="bottom">
+			    	<div className="board-inner">
+			      		<div className="box-inner">A</div>
+			      		<div className="box-inner">B</div>
+			      		<div className="box-inner">C</div>
+			      		<div className="box-inner">D</div>
+			      		<div className="box-inner">E</div>
+			      		<div className="box-inner">F</div>
+			      		<div className="box-inner">G</div>
+			      		<div className="box-inner">H</div>
+			    	</div>
+			  	</div>
+			</div>
+	  )
+
+	}
 }
 
-export default ChessBoard;
+
+	
