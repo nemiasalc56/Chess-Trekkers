8669f4391e0b061058a6e1256349cf8a391d52db
roberto cortes <robertcortesgrimm223@gmail.com>
2020-05-07 20:45:58 -0500

    setup home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import ChessBoard from './ChessBoard';
+// import ChessBoard from './ChessBoard';
+import HomeComponent from './HomeComponent'
+
 
 function App() {
   return (
     <div className="App">
-    	Chess Trekkers
-    <ChessBoard />
+    	<HomeComponent/>
     </div>
   )
 }
--- src/ChessBoard/index.js
+++ src/ChessBoard/index.js
@@ -61,7 +61,7 @@ export default class ChessBoard extends Component {
 				<div className="box"></div>
 				<div className="box"></div>
 				<div className="box"></div>
-				<div className="box"></div>
+				<div className="inbox"></div>
 
 				<div className="box"></div>
 				<div className="box"></div>
