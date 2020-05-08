b81b061e8bb910618443c9f2f27f4d9e47ffe769
roberto cortes <robertcortesgrimm223@gmail.com>
2020-05-07 20:47:52 -0500

    made app a smart component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 // import ChessBoard from './ChessBoard';
 import HomeComponent from './HomeComponent'
 
 
-function App() {
-  return (
+export default class App extends Component {
+  constructor(){
+  	super()
+  }
+  render(){
+  	return (
     <div className="App">
     	<HomeComponent/>
     </div>
   )
+  } 
 }
 
-export default App;
+
